4b740d9873ea10eb4db2d076c872c07a55eae4a1
jkrusina <pepankrusina@gmail.com>
2023-05-15 15:36:27 +0200

    update some stuff with some stuff

--- nvim/.config/nvim/lua/josifek/packer.lua
+++ nvim/.config/nvim/lua/josifek/packer.lua
@@ -56,4 +56,13 @@ use 'mfussenegger/nvim-dap-python'
 use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
 use ( "ldelossa/nvim-dap-projects")
 use 'lewis6991/gitsigns.nvim'
+use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
 end)
