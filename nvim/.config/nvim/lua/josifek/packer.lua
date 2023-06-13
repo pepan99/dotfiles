c4a070b543a62d3f6d294b5c9c381cb8dcb0f690
jkrusina <pepankrusina@gmail.com>
2023-06-13 09:55:13 +0200

    predelal jsem zase i3 na cisla

--- nvim/.config/nvim/lua/josifek/packer.lua
+++ nvim/.config/nvim/lua/josifek/packer.lua
@@ -1,6 +1,6 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
+-- Only  required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
@@ -17,6 +17,7 @@ return require('packer').startup(function(use)
   use { 'Mofiqul/dracula.nvim', as = "dracula"}
 
   use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use 'nvim-treesitter/nvim-treesitter-context'
   use( 'nvim-treesitter/playground')
 
   use('theprimeagen/harpoon')
