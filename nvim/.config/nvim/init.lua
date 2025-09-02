118dec8a9d1d7cc6fb64535b79a2482d72ac8a4d
jkrusina <krusina.jos@gmail.com>
2025-09-02 15:36:16 +0200

    feat: update nvim/fish stuff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -738,51 +738,6 @@ require("lazy").setup({
 		end,
 	},
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-		"folke/tokyonight.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("tokyonight").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-				transparent = true, -- Enable this to disable setting the background color
-				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-				styles = {
-					-- Style to be applied to different syntax groups
-					-- Value is any valid attr-list value for `:help nvim_set_hl`
-					comments = { italic = false },
-					keywords = { italic = false },
-					-- Background styles. Can be "dark", "transparent" or "normal"
-					sidebars = "dark", -- style for sidebars, see below
-					floats = "dark", -- style for floating windows
-				},
-			})
-			-- Load the colorscheme here
-			-- vim.cmd.colorscheme("tokyonight")
-			-- vim.cmd("highlight ColorColumn ctermbg=0 guibg=#333333")
-
-			-- You can configure highlights by doing something like
-		end,
-	},
-
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				disable_background = true,
-			})
-
-			-- vim.cmd("colorscheme rose-pine")
-			vim.cmd.hi("Comment gui=none")
-		end,
-	},
 	{
 		{
 			"catppuccin/nvim",
@@ -790,7 +745,7 @@ require("lazy").setup({
 			priority = 1000,
 			config = function()
 				require("catppuccin").setup({
-					flavour = "macchiato", -- latte, frappe, macchiato, mocha
+					flavour = "auto", -- latte, frappe, macchiato, mocha
 					background = { -- :h background
 						light = "latte",
 						dark = "mocha",
@@ -832,8 +787,7 @@ require("lazy").setup({
 						-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 					},
 				})
-
-				vim.cmd("colorscheme catppuccin-macchiato")
+				vim.cmd.colorscheme("catppuccin")
 				vim.cmd.hi("Comment gui=none")
 			end,
 		},
